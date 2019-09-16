cf2bb288f78e79914f7af9d9630133b667219aa8
Níkolas Vargas <vargasnikolass@gmail.com>
2019-09-16 19:05:31 -0300

    added create_file function

--- source/directory.py
+++ source/directory.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from pathlib import Path
+from typing import Callable
 
 logging.basicConfig(format='%(levelname)s: %(message)s')
 
@@ -8,11 +9,11 @@ INPUT_DIRECTORY: str = 'data/in'
 OUTPUT_DIRECTORY: str = 'data/out'
 
 
-def create_inpath():
+def create_inpath() -> Callable:
     return _create_dir(INPUT_DIRECTORY)
 
 
-def create_outpath():
+def create_outpath() -> Callable:
     return _create_dir(OUTPUT_DIRECTORY)
 
 
@@ -25,3 +26,20 @@ def _create_dir(dir_name: str) -> (str, None):
         return False
 
     os.makedirs(path)
+
+
+def create_file(filename: str, data: str):
+    """
+    Cria um arquivo {filename}.done.dat no OUTPUT_DIRECTORY definido
+    Escreve no arquivo os dados que foram passados por argumenot
+    """
+    path: str = Path.home() / OUTPUT_DIRECTORY
+
+    if path.exists():
+        fullpath: str = f"{path}/{filename}.done.dat"
+
+    create_outpath()
+    fullpath: str = f"{path}/{filename}.done.dat"
+
+    with open(fullpath, 'w') as new_file:
+        new_file.write(str(data))
--- source/main.py
+++ source/main.py
@@ -1,5 +1,7 @@
 from directory import create_inpath, create_outpath
+from files import process_file
 
 if __name__ == '__main__':
     create_inpath()
     create_outpath()
+    process_file()
