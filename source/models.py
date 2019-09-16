d40800f6c2629dc5c33f080cd96e98d03f9ab54e
Níkolas Vargas <vargasnikolass@gmail.com>
2019-09-16 01:48:19 -0300

    working on parser

--- source/models.py
+++ source/models.py
@@ -1,15 +1,72 @@
-from collections import namedtuple
+class Seller:
+    _CODE: int = 1
+    _sellers: int = 0
 
-Seller = namedtuple('Seller', 'name cpf salary')
-Customer = namedtuple('Customer', 'name cnpj BusinessArea')
+    def __init__(self, name: str, cpf: int, salary: float):
+        self.name = name
+        self.cpf = cpf
+        self.salary = salary
+        Seller._sellers += 1
+
+    @classmethod
+    def get_sellers(cls) -> int:
+        return cls._sellers
+
+    @classmethod
+    def get_code(cls) -> int:
+        return cls._CODE
+
+
+class Customer:
+    _CODE = 2
+    _customers: int = 0
+
+    def __init__(self, name: str, cnpj: int, business_area: str):
+        self.name = name
+        self.cnpj = cnpj
+        self.business_area = business_area
+        Customer._customers = 1
+
+    @classmethod
+    def get_customers(cls):
+        return cls._customers
+
+    @classmethod
+    def get_code(cls) -> int:
+        return cls._CODE
 
 
 class Order:
-    def __init__(self, id: int, seller_name: str):
-        self.id = id
-        self.seller = seller_name
+    _CODE = 3
+
+    def __init__(self, id: int, salesman_name: str):
+        self.id: int = id
+        self.salesman_name: int = salesman_name
+
+    def __repr__(self) -> str:
+        fmt = "{}(id={}, salesman_name={!r})"
+        return fmt.format(
+            self.__class__.__name__,
+            self.id,
+            self.salesman_name
+        )
+
+    @classmethod
+    def get_code(cls) -> int:
+        return cls._CODE
 
 
 class OrderItem:
-    def __init__(self):
-        pass
+    def __init__(self, id: int, quantity: int, price: float):
+        self.id: int = id
+        self.quantity: int = quantity
+        self.price: float = price
+
+    def __repr__(self) -> str:
+        fmt: str = "{}(id={}, quantity={}, price={})"
+        return fmt.format(
+            self.__class__.__name__,
+            self.id,
+            self.quantity,
+            self.price
+        )
--- source/parser.py
+++ source/parser.py
@@ -1,2 +1,97 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+
+import logging
+import re
+
+from glob import glob
+from os.path import splitext
+from pathlib import Path
+from typing import BinaryIO, List, Set, AnyStr
+
+from directory import INPUT_DIRECTORY
+from models import Seller, Customer, Order, OrderItem
+
+
+INPUT_PATH: str = Path.home() / INPUT_DIRECTORY
+SEPARATOR: str = 'ç'
+FILE_EXTENSION: str = '.dat'
+
+logging.getLogger().setLevel(logging.INFO)
+
+
+def search_batch_files() -> List:
+    logging.info(f"loading all .dat files in {INPUT_PATH}")
+    return glob(f"{INPUT_PATH}/*{FILE_EXTENSION}")
+
+
+def read_large_file(file_object: BinaryIO):
+    """
+    Generator to read a large file(lazily)
+    """
+    while True:
+        data: AnyStr = file_object.readline()
+        if not data:
+            break
+        yield data
+
+
+def process_file() -> None:
+    for pathfile in search_batch_files():
+        dataset: Set = set()
+        try:
+            with open(pathfile) as file_handler:
+                for line in read_large_file(file_handler):
+                    dataset.update(r for r in line.splitlines())
+        except (IOError, OSError) as e:
+            logging.ERROR(f"Something is wrong: {e}")
+
+        parse_data(dataset)
+
+
+def process_all_files() -> Set:
+    rows: Set = set()
+    for pathfile in search_batch_files():
+        try:
+            with open(pathfile) as file_handler:
+                for line in read_large_file(file_handler):
+                    rows.update(r for r in line.splitlines())
+        except (IOError, OSError) as e:
+            logging.ERROR(f"Something is wrong: {e}")
+    return rows
+
+
+def parse_data(dataset: Set):
+    # Slice object for check IDS
+    ID: slice = slice(0, 3)
+
+    # Sale regex
+    OLD_REGEX = re.compile(r'-(\d+)-([0-9]*\.?[0-9]*)[,]?')
+    SALE_REGEX = re.compile(r'([0-9][,]?)-(\d+)-([0-9]*\.?[0-9]*)[,]?')
+
+    CPF = CPNJ = 1
+    SELLER_NAME = CUSTOMER_NAME = ORDER_ITEMS = 2
+    SALARY = BUSINESS_AREA = SALES_MAN = 3
+
+    data: List = list(dataset)
+    number_of_sellers = number_of_customers = most_expensive_sale_id = worst_seller_id = int()
+
+    for row in data:
+        line = row.split(SEPARATOR)
+        if int(row[ID]) == Seller.get_code():
+            print(line)
+            number_of_sellers += 1
+        elif int(row[ID]) == Customer.get_code():
+            number_of_customers += 1
+        else:
+            print(line)
+
+            sale_id, sale_sum = (int(), float())
+            for line_id, quantity, price in SALE_REGEX.findall(line[ORDER_ITEMS]):
+                sale_id = line_id
+                sale_sum += float(quantity) * float(price)
+
+
+if __name__ == "__main__":
+    data: List = list(process_all_files())
+    process_file()
