2a711fed14a6c6e77c389c17ccfcff722355702e
Níkolas Vargas <vargasnikolass@gmail.com>
2019-09-17 12:21:19 -0300

    main loop watching input path of .dat files

--- source/main.py
+++ source/main.py
@@ -1,7 +1,19 @@
-from directory import create_inpath, create_outpath
+from pathlib import Path
+from watchgod import watch
+
+from directory import create_inpath, create_outpath, INPUT_DIRECTORY
 from files import process_file
+
 
 if __name__ == '__main__':
     create_inpath()
     create_outpath()
     process_file()
+
+    outpath: str = Path.home() / INPUT_DIRECTORY
+    delete_event: str = 'Change.deleted'
+
+    for changes in watch(outpath):
+        for file in changes:
+            if delete_event not in str(file[0]):
+                process_file(file[1])
